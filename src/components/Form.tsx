a49e2018693cad37736b1bec7b3ad6fcbf2c1626
Tashmax19 <tashma19@gmail.com>
2021-05-21 18:43:19 -0700

    change to original

---
+++ src/components/Form.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+// GENERICS IN TYPESCRIPT ...
+// useEffect,useState
+// Form Component => Function based component
+const Form = (props:any) => {
+  // useState
+  const [todo, setTodo] = useState("");
+ 
+
+  const Submit = (event: any) => {
+    event.preventDefault();
+  
+    props.setTodos([...props.mytodos,todo])
+  };
+
+  const handleChange = (event: any) => {
+    setTodo(event.target.value);
+    
+  };
+  return (
+    <form  onSubmit={Submit}>
+      <input type="text" onChange={handleChange} className="form-control form-control-sm w-50" />
+      <button type="submit" className="btn btn-info btn-sm text-light"> Add</button>
+    </form>
+  );
+};
+
+export default Form;
