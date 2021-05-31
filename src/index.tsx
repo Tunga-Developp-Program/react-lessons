05d4ad06b207724d281866da1a9839ba12bf16d5
ericmuh <muhwezi1000@gmail.com>
2021-05-31 16:22:28 +0300

    some update

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+
 // container for the application
-const container = document.querySelector("#app");
+const container = document.querySelector("#root");
 ReactDOM.render(<App />, container);
